b76bdbefc23b002bb7796344ade93fb2aac3c4ae
Brent Bailey <baileyb@alumni.reed.edu>
2016-03-28 19:51:09 -0400

    basic socket chat functionality

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express')(),
+http = require('http').Server(app),
+app = express(),
+io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-	res.send('<h1>Works!</h1>');
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('a user has connected');
+	socket.on('chat message', function(msg) {
+		console.log('message:' + msg);
+		io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
 });
 
 http.listen(3000, function(){
