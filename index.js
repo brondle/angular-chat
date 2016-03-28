55ccc3174934f0df90c937370856fa612f476edc
Brent Bailey <baileyb@alumni.reed.edu>
2016-03-28 19:55:35 -0400

    fix bugginess

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var express = require('express')(),
-http = require('http').Server(app),
-app = express(),
-io = require('socket.io')(http);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
